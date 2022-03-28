7a8dc1b79c3a3b1f844e08c97f72530f5363fb69
Dylan Peterson <dylan.peterson17@gmail.com>
2022-03-28 15:30:37 -0700

    WIP: Break the webpage in disatrous proportions
    Co-authored-by: Mark McConnell <amark.mcconnell@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -8,13 +8,32 @@ $(document).ready(function () {
     const verbInput = $("input#verb").val();
     const nounInput = $("input#noun").val();
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
 
-    $("#story").show();
+  const inputs = [person1Input,person2Input,animalInput,exclamationInput,verbInput,nounInput];
+  $("#story1").text("")
+  inputs.forEach(function(input) {
+  $("#story1").append("One day, " + input[0] + " and " + input + " were walking through the woods, when suddenly a giant " + input + " appeared. " + input + ' "' + input[0] + '",' + input[1] + " cried. The two of them" + input + " as quickly possible, and when they were safe," + input[0] + " and" + " " + input[1] +  " gave each other a giant " + input + " .")
+})
+
+    // $("#story").show();
   });
 });
+
+// const inputs = [person1Input,person2Input,animalInput,exclamationInput,verbInput,nounInput];
+// inputs.forEach(function(input) {
+//   $("story").append("One day, " + input[0] + " and " + input + " were walking through the woods, when suddenly a giant " + input + " appeared. " + input + '"' + input[0] + '",' + input[1] + " cried. The two of them" + input + " as quickly possible, and when they were safe," + input[0] + " and" + input[1] +  " gave each other a giant" + input + " .")
+// })
+
+
+// const arrayOfThingsILike = ["bubble baths", "kittens", "good books", "clean code"];
+// arrayOfThingsILike.forEach(function(thing) {
+//   $("#likable-things").append(" " + thing + "!");
+// });
+
+
+    // $(".person1").text(person1Input);
+    // $(".person2").text(person2Input);
+    // $(".animal").text(animalInput);
+    // $(".exclamation").text(exclamationInput);
+    // $(".verb").text(verbInput);
+    // $(".noun").text(nounInput);
